fe6e881597d590263d12972b74147c5dcb2fcc22
Kreiger444 <virinchipeddireddy@gmail.com>
2023-02-11 16:33:11 +0530

    C version completed

--- DBMS_Lab/Assignment_3/dbmsc.c
+++ DBMS_Lab/Assignment_3/dbmsc.c
@@ -1,106 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <libpq-fe.h>
+#include <string.h>
+#include <mysql/mysql.h>
 
-PGconn *conn;
+int main(){
 
-void executeQuery1() {
-  PGresult *res;
+    MYSQL *connection;
+    MYSQL_RES *answer;
+    MYSQL_ROW row;
 
-  res = PQexec(conn, "SELECT * FROM table1");
-  if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-    printf("Query 1 failed: %s\n", PQerrorMessage(conn));
-    PQclear(res);
-    return;
-  }
+    char *server= "10.5.18.70";
+    char *username= "20CS10075";
+    char *password= "20CS10075";
+    char *database= "20CS10075";
+    int port= 3306;
 
-  int nfields = PQnfields(res);
-  int ntuples = PQntuples(res);
+    char *queries[13];
 
-  printf("Query 1 results:\n");
-  printf("----------------\n");
-  printf("Number of fields: %d\n", nfields);
-  printf("Number of tuples: %d\n", ntuples);
+    queries[0]="select p.name from physician as p join trained_in as t on t.physician=p.employeeid join procedures as pr on pr.code=t.treatment where pr.name= 'bypass surgery';";
+    queries[1]="select p.name from physician  p join trained_in t on p.employeeid = t.physician join procedures pr on t.treatment = pr.code join affiliated_with a on p.employeeid = a.physician join department d on a.department = d.departmentid where pr.name = 'bypass surgery' and d.name = 'cardiology';";
+    queries[2]="select n.name from nurse n join room as r on r.number=123 join on_call as oc on (oc.blockcode, oc.blockfloor)=(r.blockcode, r.blockfloor) where oc.nurse=n.employeeid;";
+    queries[3]="select p.name, p.address from patient p join medication as med on med.name= 'remdesivir' join prescribes as pres on pres.medication=med.code and pres.patient=p.ssn;";
+    queries[4]="select p.name, p.insuranceid from patient p join room as r on r.type = 'icu' join stay as st on st.room=r.number and st.patient=p.ssn where timestampdiff(DAY,st.end, st.start) >=  15;";
+    queries[5]="select n.name from nurse n join undergoes as ug on ug.assistingnurse=n.employeeid join procedures as pr on pr.code=ug.procedures where pr.name='bypass surgery';";
+    queries[6]="select n.name as assisting_nurse, n.position, phys.name as physician_name from nurse n join undergoes as ug on ug.assistingnurse=n.employeeid join procedures as pr on pr.code=ug.procedures join physician as phys on phys.employeeid=ug.physician where pr.name='bypass surgery';";
+    queries[7]="select p.name from physician p join trained_in ti on ti.physician=p.employeeid join undergoes ug on ug.physician=p.employeeid where ug.procedures != ti.treatment;";
+    queries[8]="select p.name from physician as p join trained_in ti on ti.physician=p.employeeid join undergoes ug on ug.physician=p.employeeid and ug.date>ti.certificationexpires where ti.treatment=ug.procedures;";
+    queries[9]="select p.name as physician_name, ( select pr.name as procedures from procedures as pr where pr.code=ug.procedures ) as `procedure`, ug.date, ( select pt.name as patient_name from patient as pt where pt.ssn=ug.patient ) as finger from physician as p join trained_in ti on ti.physician=p.employeeid join undergoes ug on ug.physician=p.employeeid and ug.date>ti.certificationexpires where ti.treatment=ug.procedures;";
+    queries[10]="with patient_procedures as ( select ug.patient, count(*) as count from undergoes as ug join procedures on ug.procedures = procedures.code where cost > 5000 group by ug.patient ), patient_appointments as ( select appointment.patient, count(*) as appointment_count from appointment join undergoes as ug on appointment.patient = ug.patient join physician on appointment.physician = physician.employeeid join affiliated_with on appointment.physician = affiliated_with.physician join department on affiliated_with.department = department.departmentid where department.name = 'cardiology' group by appointment.patient having count(*) >= 2 ) select p.name as patient_name, ph.name as physician_name from prescribes pr join patient p on pr.patient = p.ssn join physician ph on p.pcp = ph.employeeid join patient_procedures pp on p.ssn = pp.patient join patient_appointments pa on p.ssn = pa.patient where ph.employeeid not in ( select head from department );";
+    queries[11]="with patient_medication as ( select medication, count(*) as total_prescriptions from prescribes group by medication ) select m.name, m.brand from patient_medication pm join medication m on pm.medication = m.code order by total_prescriptions desc limit 1;";
+    queries[12]="select p.name from physician as p join trained_in as t on t.physician=p.employeeid join procedures as pr on pr.code=t.treatment where pr.name= '";
 
-  for (int i = 0; i < ntuples; i++) {
-    for (int j = 0; j < nfields; j++) {
-      printf("%s\t", PQgetvalue(res, i, j));
+    connection=mysql_init(NULL);
+    
+    MYSQL *status=mysql_real_connect(connection, server, username, password, database, port, NULL, 0);
+    if(status==NULL){
+        perror("Connection Failed");
+        exit(0);
     }
-    printf("\n");
-  }
+    for(;;){
+        int querynum;
+        int columns;
+        int rows;
+        printf("Enter a Query number from 1-13 or enter -1 to exit: ");
+        scanf("%d", &querynum);
+        printf("\n");
 
-  PQclear(res);
-}
+        if(querynum==-1){
+            break;
+        }
+        else if(querynum<1 || querynum>13){
+            printf("Invalid Query number\n");
+            continue;
+        }
+        else{
+            if(querynum==13){
+                char procedure[50];
+                char *newquery;
 
-void executeQuery2() {
-  PGresult *res;
+                printf("Enter the procedure's name: ");
+                getchar();
+                fgets(procedure, 50, stdin);
+                procedure[strlen(procedure)-1]='\0';
 
-  res = PQexec(conn, "SELECT * FROM table2");
-  if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-    printf("Query 2 failed: %s\n", PQerrorMessage(conn));
-    PQclear(res);
-    return;
-  }
+                strcat(procedure, "';");
+                int newlen=strlen(procedure)+strlen(queries[12]);
 
-  int nfields = PQnfields(res);
-  int ntuples = PQntuples(res);
+                newquery=(char*)malloc((newlen+1)*sizeof(char));
 
-  printf("Query 2 results:\n");
-  printf("----------------\n");
-  printf("Number of fields: %d\n", nfields);
-  printf("Number of tuples: %d\n", ntuples);
+                strcpy(newquery, queries[12]);
+                strcat(newquery, procedure);
 
-  for (int i = 0; i < ntuples; i++) {
-    for (int j = 0; j < nfields; j++) {
-      printf("%s\t", PQgetvalue(res, i, j));
+                newquery[strlen(newquery)-1]='\0';
+                int flag=mysql_query(connection, newquery);
+                if(flag!=0){
+                    perror("could not run query");
+                    exit(0);
+                }
+                free(newquery);
+            }
+            else{
+                int flag= mysql_query(connection, queries[querynum-1]);
+                if(flag!=0){
+                    perror("could not run query");
+                    exit(0);
+                }
+            }
+        }
+        answer=mysql_store_result(connection);
+
+        columns=mysql_num_fields(answer);
+        MYSQL_FIELD *fields=mysql_fetch_fields(answer);
+        rows=mysql_num_rows(answer);
+        if(rows==0){
+            printf("Empty set\n");
+            continue;
+        }
+        int *lengths=(int*)malloc(columns*sizeof(int));
+        for(int i=0; i<columns; i++){
+            lengths[i]=0;
+        }
+        for(int i=0; i<columns; i++){
+            int temp=strlen(fields[i].name);
+            if(temp>lengths[i]){
+                lengths[i]=temp;
+            }
+        }
+        while((row=mysql_fetch_row(answer))!=NULL){
+            for(int i=0; i<columns; i++){
+                int temp=strlen(row[i]);
+                if(temp>lengths[i]){
+                    lengths[i]=temp;
+                }
+            }
+        }
+        printf("||");
+        for(int i=0; i< columns; i++){
+            printf(" %-*s ||", lengths[i], fields[i].name);
+
+        }
+        printf("\n");
+
+        mysql_data_seek(answer,0);
+        while((row=mysql_fetch_row(answer))){
+            printf("||");
+            for(int i=0; i<columns; i++){
+                printf(" %-*s ||", lengths[i], row[i]);
+            }
+            printf("\n");
+        }
+        free(lengths);
     }
-    printf("\n");
-  }
 
-  PQclear(res);
-}
-
-int main() {
-  int choice;
-
-  conn = PQconnectdb("host=hostname port=5432 dbname=database_name user=username password=password");
-  if (PQstatus(conn) != CONNECTION_OK) {
-    fprintf(stderr, "Connection to database failed: %s\n", PQerrorMessage(conn));
-    exit(1);
-  }
-
-  printf("Connected to the database\n");
-  printf("========================\n");
-  printf("Choose one of the following options:\n");
-  printf("1. Query 1\n");
-  printf("2. Query 2\n");
-  printf("3. Exit\n");
-  printf("Enter your choice: ");
-  scanf("%d", &choice);
-
-  while (choice != 3) {
-    switch (choice) {
-      case 1:
-        executeQuery1();
-        break;
-      case 2:
-        executeQuery2();
-        break;
-      default:
-        printf("Invalid choice. Try again\n");
-        break;
-    }
-
-    printf("\n\n");
-    printf("Choose one of the following options:\n");
-    printf("1. Query 1\n");
-    printf("2. Query 2\n");
-    printf("3. Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-  }
-
-  PQfinish(conn);
-
-  return 0;
+    mysql_close(connection);
+    return 0;
 }
